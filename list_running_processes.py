f3af4b45cfb6f0aa2ef9101149e418160397d4c4
Ben199810 <ben29774583@gmail.com>
2024-12-23 13:48:32 +0800

    feat: 取消篩選特定名稱進程

--- list_running_processes.py
+++ list_running_processes.py
@@ -7,8 +7,9 @@ def list_running_processes():
         pid = info['pid']
         name = info['name']
         user = info['username']
-        if 'php' in name.lower():
-          print(f"PID: {pid}, 程式名稱: {name}, 使用者: {user}")
+        # if 'php' in name.lower():
+        #   print(f"PID: {pid}, 程式名稱: {name}, 使用者: {user}")
+        print(f"PID: {pid}, 程式名稱: {name}, 使用者: {user}")
       except (psutil.NoSuchProcess, psutil.AccessDenied,):
         pass
 
